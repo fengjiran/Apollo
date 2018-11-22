51d78805c87435beaf7936bf30db4e93a6b76959
fengjiran <fengjiran@foxmail.com>
2018-11-22 18:40:19 +0800

    add constructors

--- header/types.h
+++ header/types.h
@@ -5,4 +5,16 @@
 #ifndef APOLLO_TYPES_H
 #define APOLLO_TYPES_H
 
+namespace cv {
+    template<typename _Tp>
+    class Size_ {
+        typedef _Tp value_type;
+
+        Size_();
+        Size_(_Tp _width, _Tp _height);
+        Size_(const Size_& sz);
+
+    };
+}
+
 #endif //APOLLO_TYPES_H
