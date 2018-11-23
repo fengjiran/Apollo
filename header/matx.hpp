d141a2cb3493343b968562f0e74e95f76c0acb2a
fengjiran <fengjiran@foxmail.com>
2018-11-23 20:39:15 +0800

    add members

--- header/matx.hpp
+++ header/matx.hpp
@@ -2,6 +2,7 @@
 // Created by richard on 2018/11/23.
 //
 
+
 #ifndef APOLLO_MATX_HPP
 #define APOLLO_MATX_HPP
 
@@ -19,7 +20,33 @@ namespace apollo {
         typedef Matx<_Tp, m, n> mat_type;
         typedef Matx<_Tp, shortdim, 1> diag_type;
 
+        Matx();
+
+        Matx(_Tp v0);
+
+        _Tp val[m * n]; // matrix elements
+
+
     };
+
+    ////////////////////// Matx Implementation /////////////////////////
+    template<typename _Tp, int m, int n>
+    inline
+    Matx<_Tp, m, n>::Matx() {
+        for (int i = 0; i < channels; i++)
+            val[i] = _Tp(0);
+    }
+
+    template<typename _Tp, int m, int n>
+    inline
+    Matx<_Tp, m, n>::Matx(_Tp v0) {
+        val[0] = v0;
+        for (int i = 1; i < channels; i++)
+            val[i] = _Tp(0);
+
+    }
+
+
 }
 
 
